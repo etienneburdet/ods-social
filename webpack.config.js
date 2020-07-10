57567d4c0e3fe46169708573cdbf06a92b9b2419
Etienne Burdet <etienneburdet@users.noreply.github.com>
2020-07-10 15:26:18 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { InjectManifest } = require('workbox-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/app.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'ODS Minimal template',
+      template: 'public/index.html'
+    }),
+    new InjectManifest({
+      swSrc: './src/sw.js',
+      swDest: './sw.js',
+      maximumFileSizeToCacheInBytes: 5000000
+    }),
+    new CopyPlugin(
+      [
+        { from: 'src/img', to: 'img/' },
+        'src/manifest.webmanifest'
+      ]
+    )
+  ],
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].[contenthash].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+}
